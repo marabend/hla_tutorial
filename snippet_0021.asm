5a8d312aa2aec11f26d0154b3c6a2b7e02fb8ab3
marabend <143610328+marabend@users.noreply.github.com>
2024-05-31 08:21:17 +0200

    Create snippet_0021.asm

---
+++ snippet_0021.asm
@@ -0,0 +1,15 @@
+program HexIO;
+
+#include ("stdlib.hhf");
+
+static
+	i32: int32;
+	
+begin HexIO;
+	stdout.put( "Enter a hexadecimal value: ");
+	stdin.getd(); // doesn't seem to work properly
+	mov(eax, i32);
+	stdout.put(" The value you entered was $");
+	stdout.putd( i32 );
+	stdout.newln();
+end HexIO;
