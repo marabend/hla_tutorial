d86eee91f20c3ed7c53f0d3ed546580b2a9712c1
marabend <143610328+marabend@users.noreply.github.com>
2024-06-04 09:20:21 +0200

    Create snippet_0031.asm

---
+++ snippet_0031.asm
@@ -0,0 +1,25 @@
+// snippet - Example of memory alignment
+
+static(4)
+	dw: dword;
+	b: byte;
+	w: word;
+	dw2: dword;
+	w2: word;
+	b2: byte;
+	dw3: dword;
+	
+static(4)
+	dw: dword;
+	b: byte;
+	
+static(2)
+	w: word;
+	
+static(4)
+	dw2: dword;
+	w2: word;
+	b2: byte;
+
+static(4)
+	dw3: dword;
