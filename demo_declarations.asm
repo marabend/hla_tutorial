83d1d7fd17283416bac723a9a375cf443063ba98
marabend <143610328+marabend@users.noreply.github.com>
2024-06-05 11:01:59 +0200

    Create demo_declarations.asm

---
+++ demo_declarations.asm
@@ -0,0 +1,23 @@
+program DemoDeclarations;
+	static
+		i_static: int32;
+	var
+		i_auto: int32;
+	storage
+		i_uninit: int32;
+	readonly
+		i_readonly: int32 := 5;
+	static
+		j: uns32;
+	var
+		k:char;
+	readonly
+		i2:uns8 := 9;
+	storage
+		c:char;
+	storage
+		d:dword;
+	
+	begin DemoDeclarations;
+		// code goes here
+	end demoDeclarations;
